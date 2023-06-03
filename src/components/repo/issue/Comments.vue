d2ce092821a0e4659487ccea4a10af2df79c88ad
Jonathan Martins <85448311+dejmartins@users.noreply.github.com>
2023-06-03 08:38:47 +0100

    Comments component

---
+++ src/components/repo/issue/Comments.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <div>
+            <h4>Comments</h4>
+            <div v-for="c of comments" :key="c.id">
+                {{ c.user && c.user.login }} - {{ c.body }}
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import { octokitMixins } from "../../../mixins/octokitMixin"
+
+    export default{
+        name: "IssueComments",
+        props : {
+            owner: {
+                type: String,
+                required: true
+            },
+            repo: {
+                type: String,
+                required: true
+            },
+            issueNumber: {
+                type: Number, 
+                required: true
+            }
+        },
+        data(){
+            return {
+                comments: []
+            }
+        }, 
+        mixins: [octokitMixins],
+        methods: {
+            async getIssueComments(owner, repo, issueNumber){
+                if( 
+                    typeof owner != "string" ||
+                    typeof repo != "string" || 
+                    typeof issueNumber != "number"){
+                    return;
+                }
+
+                const octokit = this.createOctokitClient();
+                const {data: comments} = await octokit.issues.listComments({
+                    owner, repo, issue_number: issueNumber
+                })
+
+                this.comments = comments
+            }
+
+        }
+    }
+</script>
---
+++ src/components/repo/issue/Issues.vue
@@ -0,0 +1,36 @@
+<template>
+
+</template>
+<script>
+    import { octokitMixin } from '../../../mixins/octokitMixin';
+    import IssueComments from "./../issue/Comments.vue"
+
+    export default{
+        name: "RepoIssues",
+        components: {
+            IssueComments,
+        },
+        props: {
+            owner: {
+                type: String,
+                required: true
+            },
+            repo: {
+                type: String,
+                required: true
+            },
+        },
+        mixins: [octokitMixin],
+        data(){
+            return{
+                issues: [],
+                showIssues: false
+            }
+        },
+        methods: {
+            async getRepoIssues(owner, repo){
+                const octokit = this.createOctokitClient();
+            }
+        }
+    }
+</script>
