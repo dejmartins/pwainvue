73cb73a85e48ef24f8c081e56b23826f5aae9acd
Jonathan Martins <85448311+dejmartins@users.noreply.github.com>
2023-06-03 08:54:45 +0100

    Issues component

--- src/components/repo/issue/Issues.vue
+++ src/components/repo/issue/Issues.vue
@@ -1,5 +1,16 @@
 <template>
-
+    <div v-if="issues.length > 0">
+        <button @click="showIssues = !showIssues">
+            {{ showIssues ? 'Hide' : 'Show' }}
+        </button>
+        <div v-if="showIssues">
+            <div v-for="i of issues" :key="i.id">
+                <h3>{{ i.title }}</h3>
+                <a :href="i.url">Go to the issue</a>
+                <IssueComments :owner="owner" :repo="repo" :issue-number="i.number"/>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
     import { octokitMixin } from '../../../mixins/octokitMixin';
@@ -30,7 +41,27 @@
         methods: {
             async getRepoIssues(owner, repo){
                 const octokit = this.createOctokitClient();
+                const { data: issues } = await octokit.issues.listForRepo({
+                    owner, repo
+                });
+                this.issues = issues
             }
+        },
+        watch: {
+            owner: {
+                handler(val){
+                    this.getRepoIssues(val, this.repo)
+                }
+            }, 
+
+            repo: {
+                handler(val){
+                    this.getRepoIssues(this.owner, val)
+                }
+            }
+        },
+        created(){
+            this.getRepoIssues(this.owner, this.repo)
         }
     }
 </script>
